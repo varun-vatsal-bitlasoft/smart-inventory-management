0762b8c4018aa97572fbb6a7ac6f68c3233519d3
varun vatsal <varunvatsal@bitlasoft.com>
2025-03-17 15:20:19 +0530

    changed name of password to password_digest for bcrypt to work

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_03_17_062551) do
+ActiveRecord::Schema[7.1].define(version: 2025_03_17_094806) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -88,7 +88,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_03_17_062551) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
-    t.text "password"
+    t.text "password_digest"
     t.string "email"
     t.integer "mobile"
     t.integer "department_id", null: false
