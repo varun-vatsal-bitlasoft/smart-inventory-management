71ff24ccbe721583c739e3aaa1c191c49eea4ad0
varun vatsal <varunvatsal@bitlasoft.com>
2025-03-22 20:14:04 +0530

    restricted options for non admin user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
+    def user_is_admin?
+        RoleDescription.find(User.find(session[:user_id]).role_description_id).name == "admin"
+    end
 
+    helper_method :user_is_admin? 
 end
